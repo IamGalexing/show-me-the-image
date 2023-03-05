3af727bcdb172f2c82bc39f7ea7c8f5117804763
Oleksandr Gneushev <galexing@gmail.com>
2023-03-05 11:41:50 +0100

    add link to github portfolio in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,11 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Made by @IamGalexing with
+      Made by&nbsp;
+      <a href='https://github.com/IamGalexing' target='_blank' rel='noreferrer'>
+        @IamGalexing
+      </a>
+      &nbsp;with
       <span role='img' aria-label='heart'>
         ♥
       </span>
